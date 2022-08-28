c4994eb827974978cd75d8599feaa9b7db0a9158
Tomasz Kowacki <tomek.kowacki@gmail.com>
2022-08-28 17:53:43 +0200

    First commit

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodilla.calculator;
+
+public class Calculator {
+    public double add(double a, double b) {
+        return a + b;
+    }
+    public double subtract(double a, double b) {
+        return b - a;
+    }
+}
